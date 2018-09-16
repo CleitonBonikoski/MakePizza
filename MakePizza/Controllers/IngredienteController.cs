a29866109c91e79bf2d950c73a7e3b795fbae45b
cleiton <cleitonbonikoski57@gmail.com>
2018-09-16 19:57:01 -0300

    Removendo Using IngredienteController

--- MakePizza/Controllers/IngredienteController.cs
+++ MakePizza/Controllers/IngredienteController.cs
@@ -1,9 +1,5 @@
 ﻿using MakePizza.DAO;
 using MakePizza.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MakePizza.Controllers
