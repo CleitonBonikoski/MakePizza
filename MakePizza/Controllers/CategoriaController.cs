1812acd9b2b79b47b40ac03e2f6c9d47f4066822
Esdras Rocha <esdrasrocha1122@gmail.com>
2018-09-18 15:36:24 -0300

    Finalizando CRUD de categorias

--- MakePizza/Controllers/CategoriaController.cs
+++ MakePizza/Controllers/CategoriaController.cs
@@ -13,7 +13,7 @@ namespace MakePizza.Controllers
 		#region Home()
 		public ActionResult Home()
 		{
-			return View();
+            return View(CategoriaDAO.RetornarCategorias());
 		}
 		#endregion
 
@@ -33,6 +33,51 @@ namespace MakePizza.Controllers
 				
 			return View();
 		}
-		#endregion
-	}
+        #endregion
+
+        #region RemoverCategoria(id)
+        public ActionResult RemoverCategoria(int id)
+        {
+            CategoriaDAO.RemoverCategoria(id);
+            return RedirectToAction("Home", "Categoria");
+        }
+        #endregion
+
+        #region AlterarCategoria(id)
+        public ActionResult AlterarCategoria(int id)
+        {
+            return View(CategoriaDAO.BuscarCategoriaPorId(id));
+        }
+        #endregion
+
+        #region HttpPost Alterar Categoria(Categoria)
+        [HttpPost]
+        public ActionResult AlterarCategoria(Categoria categoriaAlterada)
+        {
+            Categoria categoriaOriginal =
+                CategoriaDAO.BuscarCategoriaPorId(categoriaAlterada.IdCategoria);
+
+            categoriaOriginal.NomeCategoria = categoriaAlterada.NomeCategoria;
+            categoriaOriginal.DescricaoCategoria = categoriaAlterada.DescricaoCategoria;
+            categoriaOriginal.StatusCategoria = categoriaAlterada.StatusCategoria;
+
+            if (ModelState.IsValid)
+            {
+                if (CategoriaDAO.AlterarCategoria(categoriaOriginal))
+                {
+                    return RedirectToAction("Home", "Categoria");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Não é possível alterar uma categoria com o mesmo nome!");
+                    return View(categoriaOriginal);
+                }
+            }
+            else
+            {
+                return View(categoriaOriginal);
+            }
+        }
+        #endregion
+    }
 }
--- MakePizza/DAO/CategoriaDAO.cs
+++ MakePizza/DAO/CategoriaDAO.cs
@@ -1,50 +1,74 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using MakePizza.Models;
 
 namespace MakePizza.DAO
 {
-	public class CategoriaDAO
-	{
-		#region Contexto
-		private static Contexto contexto = SingletonContexto.GetInstance();
-		#endregion
+    public class CategoriaDAO
+    {
+        #region Contexto
+        private static Contexto contexto = SingletonContexto.GetInstance();
+        #endregion
 
-		#region CadastrarCategoria(Categoria)
-		public static bool CadastrarCategoria(Categoria categoria)
-		{
-			if (BuscarCategoriaPorNome(categoria) == null)
-			{
-				contexto.Categorias.Add(categoria);
-				contexto.SaveChanges();
-				return true;
-			}
-			return false;
-		}
-		#endregion
+        #region CadastrarCategoria(Categoria)
+        public static bool CadastrarCategoria(Categoria categoria)
+        {
+            if (BuscarCategoriaPorNome(categoria) == null)
+            {
+                contexto.Categorias.Add(categoria);
+                contexto.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        #endregion
 
-		#region RetornarCategorias()
-		public static List<Categoria> RetornarCategorias()
-		{
-			return contexto.Categorias.ToList();
-		}
-		#endregion
+        #region RetornarCategorias()
+        public static List<Categoria> RetornarCategorias()
+        {
+            return contexto.Categorias.ToList();
+        }
+        #endregion
 
-		#region  BuscarCategoriaPorId(int?)
-		public static Categoria BuscarCategoriaPorId(int? idCategoria)
-		{
-			return contexto.Categorias.FirstOrDefault(_ => _.IdCategoria == idCategoria);
-		}
-		#endregion
+        #region  BuscarCategoriaPorId(int?)
+        public static Categoria BuscarCategoriaPorId(int? idCategoria)
+        {
+            return contexto.Categorias.FirstOrDefault(_ => _.IdCategoria == idCategoria);
+        }
+        #endregion
 
-		#region BuscarCategoriaPorNome(Categoria)
-		public static Categoria BuscarCategoriaPorNome(Categoria categoria)
-		{
-			return contexto.Categorias.FirstOrDefault(_ => _.NomeCategoria.Equals(categoria.NomeCategoria));
-		}
-		#endregion
-	}
+        #region  RemoverCategoria(id)
+        internal static void RemoverCategoria(int id)
+        {
+            contexto.Categorias.Remove(BuscarCategoriaPorId(id));
+            contexto.SaveChanges();
+        }
+        #endregion
+
+        #region BuscarCategoriaPorNome(Categoria)
+        public static Categoria BuscarCategoriaPorNome(Categoria categoria)
+        {
+            return contexto.Categorias.FirstOrDefault(_ => _.NomeCategoria.Equals(categoria.NomeCategoria));
+        }
+        #endregion
+
+        #region AlterarCategoria(Categoria)
+        public static bool AlterarCategoria(Categoria categoria)
+        {
+            if (contexto.Categorias.FirstOrDefault
+                (x => x.NomeCategoria.Equals(categoria.NomeCategoria) &&
+                x.IdCategoria != categoria.IdCategoria) == null)
+            {
+                contexto.Entry(categoria).State = EntityState.Modified;
+                contexto.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+        #endregion
+    }
 }
