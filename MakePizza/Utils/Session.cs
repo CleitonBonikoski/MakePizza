7e4f4f603598db95a008578ca3cdbd82aa262f4f
cleiton <cleitonbonikoski57@gmail.com>
2018-09-21 10:31:13 -0300

    Criando Session

---
+++ MakePizza/Utils/Session.cs
@@ -0,0 +1,65 @@
+﻿using MakePizza.DAO;
+using MakePizza.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakePizza.Utils
+{
+	public class Session
+	{
+		private static Contexto contexto = SingletonContexto.GetInstance();
+
+		#region Session
+		private static string CLIENTE_SESSION = "IdCliente";
+
+		private static string PEDIDO_SESSION = "GuidPedido";
+		#endregion
+
+		#region  CriarSessaoCliente()
+		public static string CriarSessaoCliente()
+		{
+			if (HttpContext.Current.Session[CLIENTE_SESSION] == null)
+			{
+				Guid guid = Guid.NewGuid();
+				HttpContext.Current.Session[CLIENTE_SESSION] = guid;
+			}
+			return HttpContext.Current.Session[CLIENTE_SESSION].ToString();
+		}
+		#endregion
+
+		#region  CriarSessaoPedido()
+		public static string CriarSessaoPedido()
+		{
+			if (HttpContext.Current.Session[PEDIDO_SESSION] == null)
+			{
+				Guid guid = Guid.NewGuid();
+				HttpContext.Current.Session[PEDIDO_SESSION] = guid;
+			}
+			return HttpContext.Current.Session[PEDIDO_SESSION].ToString();
+		}
+		#endregion
+
+		#region ValidarSessaoCliente()
+		public static string ValidarSessaoCliente()
+		{
+			if (HttpContext.Current.Session[CLIENTE_SESSION] == null)
+				return null;
+
+			return HttpContext.Current.Session[CLIENTE_SESSION].ToString();
+		}
+		#endregion
+
+		#region ValidarSessaoPedido()
+		public static string ValidarSessaoPedido()
+		{
+			if (HttpContext.Current.Session[PEDIDO_SESSION] == null)
+				return null;
+
+			return HttpContext.Current.Session[PEDIDO_SESSION].ToString();
+		}
+		#endregion
+
+	}
+}
