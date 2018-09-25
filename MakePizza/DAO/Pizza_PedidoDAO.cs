83cdf544fb9a8992b7cf634e9f7ee6f799b07dd3
cleiton <cleitonbonikoski57@gmail.com>
2018-09-24 22:46:49 -0300

    Mostrando Valor Total do Pedido

--- MakePizza/DAO/Pizza_PedidoDAO.cs
+++ MakePizza/DAO/Pizza_PedidoDAO.cs
@@ -32,6 +32,32 @@ namespace MakePizza.DAO
 		{
 			return contexto.Pizza_Pedidos.Where(_ => _.GuidPedido.Equals(sessaoPedidoAtual)).ToList();
 		}
+
+		//public static bool AtualizarPizza_Pedido( List<Pizza_Pedido> lstPizzasConfirmadas, List<Pizza_Pedido> lstPizza_Pedidos)
+		//{
+		//	try
+		//	{
+		//		//Apaga lista de pizza antiga
+		//		foreach (var pizza_pedido in lstPizza_Pedidos)
+		//		{
+		//			contexto.Pizza_Pedidos.Remove(pizza_pedido);
+		//			contexto.SaveChanges();
+		//		}
+
+		//		//Cria uma nova lista de pizza_pedido
+		//		foreach (var pizza_pedido in lstPizzasConfirmadas)
+		//		{
+		//			contexto.Pizza_Pedidos.Add(pizza_pedido);
+		//			contexto.SaveChanges();
+		//		}
+		//		return true;
+		//	}
+		//	catch (Exception)
+		//	{
+		//		return false;
+
+		//	}
+		//}
 		#endregion
 
 	}
